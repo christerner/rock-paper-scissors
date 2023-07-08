4f79b1cf40093c99523e9b782b14a8d163991247
christerner <christianuwayo250@gmail.com>
2023-07-08 16:58:00 +0200

    Added script.js and modified README.md files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # rock-paper-scissors
+
+Rock Paper Scissors
+This is my first javascript project.
+It is a simple project where I learned to work with functions, conditionals, variables and so many. As this is the beginning, I'm ready to have fun working with more advanced topics in Javascript.
---
+++ script.js
@@ -0,0 +1,76 @@
+const options = ["rock", "paper", "scissors"];
+function getComputerChoice() {
+    const choice = options[Math.floor(Math.random() * options.length)];
+    return choice;
+}
+
+let playeScore = 0;
+let computerScore = 0;
+
+// A function that plays a single round
+function playRound(playerSelection, computerSelection) {
+
+    if(playerSelection === "rock" && computerSelection === "paper") {
+        playeScore++;
+        return `You lose! ${computerSelection} beats ${playerSelection}.`;
+    
+    } else if(playerSelection === "scissors" && computerSelection === "rock") {
+        computerScore++;
+        return `You lose! ${computerSelection} beats ${playerSelection}.`;
+    
+    } else if(playerSelection === "paper" && computerSelection === "scissors") {
+        computerScore++;
+        return `You lose! ${computerSelection} beats ${playerSelection}.`;
+    
+    } else if(playerSelection ===  computerSelection) {
+        return `It's a tie! Computer played ${computerSelection} and you played ${playerSelection}.`;
+    
+    } else {
+        playeScore++;
+        return `You win! ${playerSelection} beats ${computerSelection}.`;
+    }
+    
+}
+
+// A function that gets and validates the player choice
+function getPlayerChoice() {
+    let validInput = false;
+    while (validInput == false) {
+        const choice = prompt("Enter rock, paper or scissors");
+        if (choice == null) {
+            continue;
+        }
+        const choiceToLower = choice.toLowerCase();
+        if (options.includes(choiceToLower)) {
+            validInput = true;
+            return choiceToLower;
+        }
+    }
+
+}
+
+// Main function
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection))
+
+
+    }
+    console.log("Game over!")
+    console.log("**********************");
+        
+    if (computerScore > playeScore) {
+        console.log(`You lose the game! Computer score: ${computerScore}. Your score: ${playeScore}.`)
+    }
+    else if (computerScore === playeScore) {
+        console.log(`It's a tie! Computer score: ${computerScore}. Your score: ${playeScore}.`)
+    }
+    else {
+        console.log(`You win the game! Computer score: ${computerScore}. Your score: ${playeScore}.`)
+    }
+}
+game();
+
+// END
